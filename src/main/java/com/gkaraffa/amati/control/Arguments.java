89de207205f9f8bd86849f82b43949e07490f0c6
Gabriel D. Karaffa <gk71pro@gmail.com>
2019-10-19 00:06:45 -0400

    Commit point

---
+++ src/main/java/com/gkaraffa/amati/control/Arguments.java
@@ -0,0 +1,65 @@
+package com.gkaraffa.amati.control;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.beust.jcommander.IStringConverter;
+import com.beust.jcommander.Parameter;
+
+public class Arguments {
+  @Parameter(names = {"--key", "-k"})
+  private String keyRequest;
+
+  @Parameter(names = {"--scale", "-s"})
+  private String scaleRequest;
+
+  @Parameter(names = {"--format", "-f"})
+  private String formatRequest = "text";
+
+  @Parameter(names = "--views", listConverter = ViewListConverter.class)
+  List<String> viewRequests;
+  
+  @Parameter(names = "--instruments", listConverter = ViewListConverter.class)
+  List<String> instrumentRequests;
+
+  @Parameter(names = {"--output", "-o"})
+  private String outputFileName;
+  
+  public String getKeyRequest() {
+    return keyRequest;
+  }
+
+  public String getScaleRequest() {
+    return scaleRequest;
+  }
+
+  public String getFormatRequest() {
+    return formatRequest;
+  }
+
+  public List<String> getViewRequests() {
+    return viewRequests;
+  }
+
+  public List<String> getInstrumentRequests() {
+    return instrumentRequests;
+  }
+
+  public String getOutputFileName() {
+    return outputFileName;
+  }
+
+  public class ViewListConverter implements IStringConverter<List<String>> {
+    @Override
+    public List<String> convert(String viewsString) {
+      String[] viewArray = viewsString.split(",");
+      List<String> viewList = new ArrayList<>();
+
+      for (String currentView : viewArray) {
+        viewList.add(currentView);
+      }
+
+      return viewList;
+    }
+  }
+}
--- src/main/java/com/gkaraffa/amati/control/MainController.java
+++ src/main/java/com/gkaraffa/amati/control/MainController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.IStringConverter;
-import com.gkaraffa.cremona.helper.Helper;
+import com.gkaraffa.cremona.helper.ScaleHelper;
 import com.gkaraffa.cremona.theoretical.scale.DiatonicScale;
 import com.gkaraffa.cremona.theoretical.scale.Scale;
 import com.gkaraffa.guarneri.outputform.CSVOutputFormFactory;
@@ -18,40 +18,28 @@ import com.gkaraffa.guarneri.outputform.OutputForm;
 import com.gkaraffa.guarneri.outputform.OutputFormFactory;
 import com.gkaraffa.guarneri.outputform.TabularTextOutputFormFactory;
 import com.gkaraffa.guarneri.view.ViewFactory;
+import com.gkaraffa.guarneri.view.ViewQuery;
 import com.gkaraffa.guarneri.view.ViewTable;
 import com.gkaraffa.guarneri.view.analytic.IntervalAnalyticViewFactory;
 import com.gkaraffa.guarneri.view.analytic.RomanNumeralAnalyticViewFactory;
 import com.gkaraffa.guarneri.view.analytic.ScalarAnalyticViewFactory;
 
 public class MainController {
-  @Parameter(names = {"--key", "-k"})
-  private String keyRequest;
-
-  @Parameter(names = {"--scale", "-s"})
-  private String scaleRequest;
-
-  @Parameter(names = {"--format", "-f"})
-  private String formatRequest = "text";
-
-  @Parameter(names = "--views", listConverter = ViewListConverter.class)
-  List<String> viewRequests;
-
-  @Parameter(names = {"--output", "-o"})
-  private String outputFileName;
-
+  private Arguments arguments = null;
 
   public static void main(String[] args) {
     MainController mainController = new MainController();
-    JCommander.newBuilder().addObject(mainController).build().parse(args);
-
-    mainController.run();
+    Arguments arguments = new Arguments();
+    JCommander.newBuilder().addObject(arguments).build().parse(args);
+    mainController.run(arguments);
   }
 
-  public void run() {
+  public void run(Arguments arguments) {
+    this.arguments = arguments;
     Scale scaleRendered = this.parseAndRenderScale();
-    List<ViewTable> modelsRendered = this.parseAndRenderModels(scaleRendered);
-    OutputFormFactory viewFactory = this.selectCreateViewFactory(formatRequest);
-    List<OutputForm> views = this.renderViews(modelsRendered, viewFactory);
+    List<ViewTable> modelsRendered = this.parseAndRenderAnalytics(scaleRendered);
+    OutputFormFactory viewFactory = this.selectCreateViewFactory(arguments.getFormatRequest());
+    List<OutputForm> views = this.renderAnalytics(modelsRendered, viewFactory);
 
     this.createOutput(views);
   }
@@ -63,7 +51,7 @@ public class MainController {
   }
 
   private void writeOutputToFile(List<OutputForm> views) {
-    File file = new File(this.outputFileName.trim());
+    File file = new File(arguments.getOutputFileName().trim());
     try (FileOutputStream fileOutputStream = new FileOutputStream(file);
         BufferedOutputStream writer = new BufferedOutputStream(fileOutputStream)) {
 
@@ -77,37 +65,38 @@ public class MainController {
     }
   }
 
-  private List<OutputForm> renderViews(List<ViewTable> modelsRendered, OutputFormFactory viewFactory) {
-    List<OutputForm> views = new ArrayList<OutputForm>();
+  private List<OutputForm> renderAnalytics(List<ViewTable> modelsRendered, OutputFormFactory viewFactory) {
+    List<OutputForm> analytics = new ArrayList<OutputForm>();
 
     for (ViewTable modelTable : modelsRendered) {
-      views.add(viewFactory.renderView(modelTable));
+      analytics.add(viewFactory.renderView(modelTable));
     }
 
-    return views;
+    return analytics;
   }
 
   private Scale parseAndRenderScale() {
-    Helper helper = Helper.getInstance();
-    Scale scaleRendered = helper.getScale(keyRequest, scaleRequest);
+    ScaleHelper helper = ScaleHelper.getInstance();
+    Scale scaleRendered = helper.getScale(arguments.getKeyRequest(), arguments.getScaleRequest());
 
     return scaleRendered;
   }
 
-  private List<ViewTable> parseAndRenderModels(Scale scaleRendered) {
-    List<ViewTable> modelsRendered = new ArrayList<ViewTable>();
+  private List<ViewTable> parseAndRenderAnalytics(Scale scaleRendered) {
+    List<ViewTable> viewsRendered = new ArrayList<ViewTable>();
+    List<String> viewRequests = arguments.getViewRequests();
 
-    for (String viewRequest : this.viewRequests) {
+    for (String viewRequest : viewRequests) {
       try {
         switch (viewRequest.toUpperCase().trim()) {
           case "ROMAN":
-            modelsRendered.add(getRomanNumeralModel(scaleRendered));
+            viewsRendered.add(getRomanNumeralModel(scaleRendered));
             break;
           case "INTERVAL":
-            modelsRendered.add(getIntervalModel(scaleRendered));
+            viewsRendered.add(getIntervalModel(scaleRendered));
             break;
           case "SCALAR":
-            modelsRendered.add(getScalarModel(scaleRendered));
+            viewsRendered.add(getScalarModel(scaleRendered));
             break;
         }
       }
@@ -116,13 +105,13 @@ public class MainController {
       }
     }
 
-    return modelsRendered;
+    return viewsRendered;
   }
 
   private ViewTable getRomanNumeralModel(Scale scale) throws IllegalArgumentException {
     if (scale instanceof DiatonicScale) {
-      ViewFactory modelFactory = new RomanNumeralAnalyticViewFactory();
-      return modelFactory.createModel(scale);
+      ViewFactory viewFactory = new RomanNumeralAnalyticViewFactory();
+      return viewFactory.createView(new ViewQuery(scale));
     }
     else {
       throw new IllegalArgumentException("RomanNumeralAnalytic cannot be rendered for this scale");
@@ -131,8 +120,8 @@ public class MainController {
 
   private ViewTable getIntervalModel(Scale scale) throws IllegalArgumentException {
     if (scale instanceof DiatonicScale) {
-      ViewFactory modelFactory = new IntervalAnalyticViewFactory();
-      return modelFactory.createModel(scale);
+      ViewFactory viewFactory = new IntervalAnalyticViewFactory();
+      return viewFactory.createView(new ViewQuery(scale));
     }
     else {
       throw new IllegalArgumentException("IntervalAnalytic view cannot be rendered for this scale");
@@ -140,8 +129,8 @@ public class MainController {
   }
 
   private ViewTable getScalarModel(Scale scale) {
-    ViewFactory modelFactory = new ScalarAnalyticViewFactory();
-    return modelFactory.createModel(scale);
+    ViewFactory viewFactory = new ScalarAnalyticViewFactory();
+    return viewFactory.createView(new ViewQuery(scale));
   }
 
 
@@ -158,25 +147,12 @@ public class MainController {
   }
 
   private void createOutput(List<OutputForm> views) {
+    String outputFileName = arguments.getOutputFileName();
     if ((outputFileName == null) || (outputFileName.trim().equals(""))) {
       writeOutputToStdOut(views);
     }
     else {
       writeOutputToFile(views);
-    }
-  }
-
-  public class ViewListConverter implements IStringConverter<List<String>> {
-    @Override
-    public List<String> convert(String viewsString) {
-      String[] viewArray = viewsString.split(",");
-      List<String> viewList = new ArrayList<>();
-
-      for (String currentView : viewArray) {
-        viewList.add(currentView);
-      }
-
-      return viewList;
     }
   }
 }
